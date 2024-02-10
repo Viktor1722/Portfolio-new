1cf04fc967ea79e3469cc31442d6c4909a43986e
viktor.velizarov <viktor.velizarov@bycape.io>
2024-02-10 16:05:41 +0100

    fixed the classes form class to className

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 bg-navy-blue ">
-      <div class="z-5 mx-auto">
-        <div class="container mx-auto m-20">
-          <header class="max-w-screen-sm o">
-            <h1 class="text-4xl font-bold three-d-text font-satoshi ">
-              Viktor <span class="text-sky-500">Velizarov</span>
+      <div className="z-5 mx-auto">
+        <div className="container mx-auto m-20">
+          <header className="max-w-screen-sm o">
+            <h1 className="text-4xl font-bold three-d-text">
+              Viktor <span className="text-sky-500">Velizarov</span>
             </h1>
-            <p class="text-sm mt-5">
+            <p className="text-sm mt-5">
               Fornt-end, Mobile developer & UX Designer
             </p>
           </header>
           <br />
-          <hr class="border-t border-gray-600 my-2"></hr>
-          <section id="about" className="my-10 font-satoshi">
+          <hr className="border-t border-gray-600 my-2"></hr>
+          <section id="about" className="my-10 ">
             <p>
               I'm Viktor, a 21-year-old student at Fontys ICT Eindhoven,
               specializing in Media and Design. As an ambitious front-end
@@ -25,8 +25,8 @@ export default function Home() {
             </p>
           </section>
 
-          <hr class="border-t border-gray-600 my-2"></hr>
-          <section id="skills" className="my-10 font-satoshi">
+          <hr className="border-t border-gray-600 my-2"></hr>
+          <section id="skills" className="my-10">
             <h2 className="text-2xl font-bold text-start three-d-text">
               Skills
             </h2>

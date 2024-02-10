9e49af6e06031f3a7ca2fb9d49ba441fce76edb2
viktor.velizarov <viktor.velizarov@bycape.io>
2024-02-10 16:02:26 +0100

    added most of the main information

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-navy-blue">
-      <header>
-        <b>
-          <h1 class="text-4xl font-bold three-d-text">Viktor Velizarov</h1>
-        </b>
-      </header>
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 bg-navy-blue ">
+      <div class="z-5 mx-auto">
+        <div class="container mx-auto m-20">
+          <header class="max-w-screen-sm o">
+            <h1 class="text-4xl font-bold three-d-text font-satoshi ">
+              Viktor <span class="text-sky-500">Velizarov</span>
+            </h1>
+            <p class="text-sm mt-5">
+              Fornt-end, Mobile developer & UX Designer
+            </p>
+          </header>
+          <br />
+          <hr class="border-t border-gray-600 my-2"></hr>
+          <section id="about" className="my-10 font-satoshi">
+            <p>
+              I'm Viktor, a 21-year-old student at Fontys ICT Eindhoven,
+              specializing in Media and Design. As an ambitious front-end
+              developer with a strong design sensibility, I'm committed to
+              learning and growing in the field. Known for excellent time
+              management, I prioritize work effectively.
+            </p>
+          </section>
+
+          <hr class="border-t border-gray-600 my-2"></hr>
+          <section id="skills" className="my-10 font-satoshi">
+            <h2 className="text-2xl font-bold text-start three-d-text">
+              Skills
+            </h2>
+            <div className="flex flex-row mt-4">
+              <span>Node.js</span>
+            </div>
+          </section>
+        </div>
+      </div>
     </main>
   );
 }

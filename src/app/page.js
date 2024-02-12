cb1d7fc396bdc74d630c651e78a1f4fa3265043a
viktor.velizarov <viktor.velizarov@bycape.io>
2024-02-12 13:56:40 +0100

    added more projects

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import ParticlesBackground from "./ParticlesBackground";
-import GitHub from "../../public/fonts/Github-Dark.svg";
 
 export default function Home() {
   return (
@@ -71,23 +70,121 @@ export default function Home() {
               Latest <span className="text-sky-500">Projects</span>
             </h2>
             <div className="flex flex-wrap flex-row mt-4 gap-4 cursor-pointer">
-              <div className="flex flex-row w-80 rounded-lg px-4 py-2 border border-slate-500 ">
-                <div>
-                  <span>Voice Over Generator</span>
-                  <p className="text-sm mt-2 text-slate-500">
-                    Web Development project
-                  </p>
+              <a href="https://github.com/Viktor1722">
+                <div className="flex flex-row w-80 rounded-lg px-4 py-2 border border-slate-500 items-center justify-evenly">
+                  <div className="mr-10">
+                    <span>Voice Over Generator</span>
+                    <p className="text-sm mt-2 text-slate-500">
+                      Web Development project
+                    </p>
+                  </div>
+                  <div className="border border-slate-500 p-2 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="h-4 w-4"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                      ></path>
+                    </svg>
+                  </div>
                 </div>
-                <a>
-                  <Image
-                    src={GitHub}
-                    width={50}
-                    height={50}
-                    alt="git hub icon"
-                  />
-                </a>
-              </div>
+              </a>
+
+              <a href="https://github.com/Viktor1722">
+                <div className="flex flex-row w-80 rounded-lg px-4 py-2 border border-slate-500 items-center justify-evenly">
+                  <div className="mr-10">
+                    <span>Court Vision</span>
+                    <p className="text-sm mt-2 text-slate-500">
+                      Mobile Development project
+                    </p>
+                  </div>
+                  <div className="border border-slate-500 p-2 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="h-4 w-4"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+              </a>
+
+              <a href="https://github.com/Viktor1722">
+                <div className="flex flex-row w-80 rounded-lg px-4 py-2 border border-slate-500 items-center justify-evenly">
+                  <div className="mr-10">
+                    <span>Student Corner</span>
+                    <p className="text-sm mt-2 text-slate-500">
+                      Web Development project
+                    </p>
+                  </div>
+                  <div className="border border-slate-500 p-2 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="h-4 w-4"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+              </a>
+
+              <a href="https://github.com/Viktor1722">
+                <div className="flex flex-row w-80 rounded-lg px-4 py-2 border border-slate-500 items-center justify-evenly">
+                  <div className="mr-10">
+                    <span>Honorsteine</span>
+                    <p className="text-sm mt-2 text-slate-500">
+                      Mobile Development project
+                    </p>
+                  </div>
+                  <div className="border border-slate-500 p-2 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="h-4 w-4"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+              </a>
             </div>
+          </section>
+          <hr className="border-t border-gray-600 my-2"></hr>
+          <br />
+          <section id="connect">
+            <h2 className="text-2xl font-bold text-start three-d-text">
+              Contact
+            </h2>
           </section>
         </div>
       </div>
